3565c911f00e434f1eb1f0a5a631507fa8c58959
Anicia Flores <redactorweboficial@gmail.com>
2023-09-16 19:16:55 -0300

    Se creó la Tarjeta de Perfil

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,11 @@ const toggleTheme = document.getElementById("toggle-theme");
 const toggleIcon = document.getElementById("toggle-icon");
 const toggleText = document.getElementById("toggle-text");
 
+const toggleColors = document.getElementById("toggle-colors");
+
+const rootStyles = document.documentElement.style;
+
+
 toggleTheme.addEventListener('click', ()=> {
     document.body.classList.toggle("dark");
     if(toggleIcon.src.includes("moon.svg")) {
@@ -12,3 +17,7 @@ toggleTheme.addEventListener('click', ()=> {
         toggleText.textContent = "Dark Mode";
     }
 });
+
+toggleColors.addEventListener("click", (e) => {
+    rootStyles.setProperty("--primary-color", e.target.dataset.color);
+})
